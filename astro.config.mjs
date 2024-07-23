44dafd211d3d7782229a35a743ac0c7b6a822f44
Fredey Allexiel <tu@example.com>
2024-07-22 23:50:01 -0600

    Actualiza configuración para sitio estático y favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  site: 'https://github.com/Alfmobr/PowerBuidilng.git',
-})
+  output: 'static',
+  site: 'https://power-building.vercel.app', 
+});
